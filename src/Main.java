a6a69623981f19d6fa8e51cb0e25a61218d38bd6
Oleksandr Shpak <kotmultos@gmail.com>
2022-10-24 22:15:04 +0300

    recursive isPrime added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPrime(11, 2));
+    }
+
+    private static boolean isPrime(int n, int i) {
+        if (n <= 2)
+            return (n == 2) ? true : false;
+        if (n % i == 0)
+            return false;
+        if (i * i > n)
+            return true;
+
+        return isPrime(n, i + 1);
     }
 }
